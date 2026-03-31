97f32f155a815221bb90af2621498d3b35e394af
Kiên Lê <kienle3112204@gmail.com>
2026-03-31 14:56:06 +0700

    up

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
     public static WebDriver getDriver(String browser) {
+        boolean isCI = System.getenv("CI") != null;
         WebDriver driver;
         // Kiểm tra xem có đang chạy trên GitHub Actions không
-        boolean isCI = System.getenv("CI") != null; 
 
         if (browser.equalsIgnoreCase("firefox")) {
             FirefoxOptions options = new FirefoxOptions();
@@ -18,17 +18,18 @@ public class DriverFactory {
                 options.addArguments("-headless"); // Firefox headless
             }
             driver = new FirefoxDriver(options);
-            
+
         } else { // Mặc định là Chrome
             ChromeOptions options = new ChromeOptions();
             if (isCI) {
                 options.addArguments("--headless=new"); // Chrome headless mới
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
             }
             driver = new ChromeDriver(options);
         }
-        
+
         driver.manage().window().maximize();
         return driver;
     }
