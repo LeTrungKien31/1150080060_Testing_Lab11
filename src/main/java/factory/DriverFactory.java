65d4783af1abe7e16f3d4a36fc101dc2b4e5a0c8
Kiên Lê <kienle3112204@gmail.com>
2026-03-31 14:51:14 +0700

    up

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -3,36 +3,33 @@ package factory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
+    public static WebDriver getDriver(String browser) {
+        WebDriver driver;
+        // Kiểm tra xem có đang chạy trên GitHub Actions không
+        boolean isCI = System.getenv("CI") != null; 
 
-    private static WebDriver driver;
-
-    public static WebDriver getDriver() {
-
-        if (driver == null) {
-
-            ChromeOptions options = new ChromeOptions();
-
-            // 🔥 Detect đang chạy trên CI (GitHub Actions)
-            boolean isCI = System.getenv("CI") != null; // GitHub Actions tự set CI=true
+        if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
             if (isCI) {
-                options.addArguments("--headless=new");
+                options.addArguments("-headless"); // Firefox headless
+            }
+            driver = new FirefoxDriver(options);
+            
+        } else { // Mặc định là Chrome
+            ChromeOptions options = new ChromeOptions();
+            if (isCI) {
+                options.addArguments("--headless=new"); // Chrome headless mới
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
-                options.addArguments("--window-size=1920,1080");
             }
-
             driver = new ChromeDriver(options);
         }
-
+        
+        driver.manage().window().maximize();
         return driver;
-    }
-
-    public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
     }
 }
